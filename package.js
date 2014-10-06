c9ff4826f817e7b98b2aba3d34c82a7ade7d7733
Kestanous <CMTopus@me.com>
2014-10-06 15:16:23 -0400

    init

---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  summary: " \* Fill me in! *\ ",
+  version: "0.1.0",
+  git: " \* Fill me in! *\ "
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.2');
+  api.use('kestanous:herald@0.7.0')
+  api.addFiles('lib/web-notifications.js');
+});
